6b4a3036ed37bbff8952e1142fb1516bde5a624a
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-07-08 21:08:22 +0100

    Added other taker icons

--- Wizardry/Core/SkillSys/MiscSkillEffects/PostActionSkills/TakerSkills.c
+++ Wizardry/Core/SkillSys/MiscSkillEffects/PostActionSkills/TakerSkills.c
@@ -18,6 +18,36 @@ bool PostActionTakerSkills(ProcPtr parent)
         if (SkillTester(unit, SID_TakerStrength) && gBattleActorGlobalFlag.enimy_defeated)
             SetUnitStatDebuff(unit, UNIT_STAT_BUFF_TAKER_POW);
 #endif
+
+#if defined(SID_TakerMagic) && (COMMON_SKILL_VALID(SID_TakerMagic))
+        if (SkillTester(unit, SID_TakerMagic) && gBattleActorGlobalFlag.enimy_defeated)
+            SetUnitStatDebuff(unit, UNIT_STAT_BUFF_TAKER_MAG);
+#endif
+
+#if defined(SID_TakerSkill) && (COMMON_SKILL_VALID(SID_TakerSkill))
+        if (SkillTester(unit, SID_TakerSkill) && gBattleActorGlobalFlag.enimy_defeated)
+            SetUnitStatDebuff(unit, UNIT_STAT_BUFF_TAKER_SKL);
+#endif
+
+#if defined(SID_TakerSpeed) && (COMMON_SKILL_VALID(SID_TakerSpeed))
+        if (SkillTester(unit, SID_TakerSpeed) && gBattleActorGlobalFlag.enimy_defeated)
+            SetUnitStatDebuff(unit, UNIT_STAT_BUFF_TAKER_SPD);
+#endif
+
+#if defined(SID_TakerLuck) && (COMMON_SKILL_VALID(SID_TakerLuck))
+        if (SkillTester(unit, SID_TakerLuck) && gBattleActorGlobalFlag.enimy_defeated)
+            SetUnitStatDebuff(unit, UNIT_STAT_BUFF_TAKER_LCK);
+#endif
+
+#if defined(SID_TakerDefense) && (COMMON_SKILL_VALID(SID_TakerDefense))
+        if (SkillTester(unit, SID_TakerDefense) && gBattleActorGlobalFlag.enimy_defeated)
+            SetUnitStatDebuff(unit, UNIT_STAT_BUFF_TAKER_DEF);
+#endif
+
+#if defined(SID_TakerResistance) && (COMMON_SKILL_VALID(SID_TakerResistance))
+        if (SkillTester(unit, SID_TakerResistance) && gBattleActorGlobalFlag.enimy_defeated)
+            SetUnitStatDebuff(unit, UNIT_STAT_BUFF_TAKER_RES);
+#endif
     }
 
     return true;
