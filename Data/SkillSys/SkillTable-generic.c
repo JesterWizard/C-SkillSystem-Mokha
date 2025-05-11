303daf3d64bb5c50025d393d1c75fd6722cb3bb3
JesterWizard <adapps101@gmail.com>
2025-05-11 13:55:59 +0100

    Fix - Hover box fix for 254 hp

--- Data/SkillSys/SkillTable-generic.c
+++ Data/SkillSys/SkillTable-generic.c
@@ -444,7 +444,7 @@ const struct SkillPreloadPConf gSkillPreloadPData[0x100] = {
     // // 02
     [CHARACTER_SETH] = {
         .skills = {
-            [0] = SID_Capture,
+            [0] = SID_Mimic,
         },
     },
     // // 03
--- Tools/FE-CLib-Mokha/include/player_interface.h
+++ Tools/FE-CLib-Mokha/include/player_interface.h
@@ -1,6 +1,48 @@
 #ifndef GUARD_PLAYER_INTERFACE_H
 #define GUARD_PLAYER_INTERFACE_H
 
+struct PlayerInterfaceProc
+{
+    /* 00 */ PROC_HEADER;
+
+    /* 2C */ struct Text texts[2];
+
+    /* 3C */ s8 xBurst;
+    /* 3D */ s8 yBurst;
+    /* 3E */ s8 wBurst;
+    /* 3F */ s8 hBurst;
+
+    /* 40 */ u16 * statusTm;
+    /* 44 */ s16 unitClock;
+    /* 46 */ s16 xHp;
+    /* 48 */ s16 yHp;
+    /* 4A */ u8 burstUnitIdPrev;
+    /* 4B */ u8 burstUnitId;
+    /* 4C */ u8 xCursorPrev;
+    /* 4D */ u8 yCursorPrev;
+    /* 4E */ u8 xCursor;
+    /* 4F */ u8 yCursor;
+    /* 50 */ s8 cursorQuadrant;
+    /* 51 */ u8 hpCurHi;
+    /* 52 */ u8 hpCurLo;
+    /* 53 */ u8 hpMaxHi;
+    /* 54 */ u8 hpMaxLo;
+    /* 55 */ s8 hideContents;
+    /* 56 */ s8 isRetracting;
+    /* 57 */ s8 windowQuadrant;
+    /* 58 */ int showHideClock;
+};
+
+struct PlayerInterfaceConfigEntry
+{
+    /* 00 */ s8 xTerrain, yTerrain;
+    /* 02 */ s8 xMinimug, yMinimug;
+    /* 04 */ s8 xGoal, yGoal;
+    STRUCT_PAD(0x06, 0x08);
+};
+
+extern struct PlayerInterfaceConfigEntry sPlayerInterfaceConfigLut[4];
+
 // ??? GetWindowQuadrant(???);
 // ??? GetCursorQuadrant(???);
 // ??? GetHpBarLeftTile(???);
@@ -19,7 +61,11 @@
 // ??? sub_808C360(???);
 // ??? MMB_DrawStatusText(???);
 // ??? DrawUnitDisplayHpOrStatus(???);
-// ??? InitMinimugBoxMaybe(???);
+void ApplyUnitMapUiFramePal(int faction, int palId);
+void ClearUnitMapUiStatus(struct PlayerInterfaceProc * proc, u16 * buffer, struct Unit * unit);
+void PutUnitMapUiStatus(u16 * buffer, struct Unit * unit);
+void UnitMapUiUpdate(struct PlayerInterfaceProc * proc, struct Unit * unit);
+void DrawUnitMapUi(struct PlayerInterfaceProc * proc, struct Unit * unit);
 // ??? sub_808C710(???);
 // ??? sub_808C750(???);
 // ??? sub_808C8EC(???);
