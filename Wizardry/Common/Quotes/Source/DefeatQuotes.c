c24b3a09c16024208259d70cf24142806fc18a67
JesterWizard <adapps101@gmail.com>
2025-07-06 23:30:57 +0100

    Fixed defeat quotes by adding 0xFFFF as a wildcard

--- Wizardry/Common/Quotes/Source/DefeatQuotes.c
+++ Wizardry/Common/Quotes/Source/DefeatQuotes.c
@@ -25,6 +25,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_BREGUET,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_01,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -32,6 +33,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_BONE,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_02,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -39,6 +41,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_BAZBA,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_03,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -46,6 +49,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_ENTOMBED_CH4,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_04,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -53,6 +57,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_SAAR,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_05,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -60,6 +65,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_ZONTA,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_05X,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -67,6 +73,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_NOVALA,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_06,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -74,6 +81,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_MURRAY,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_07,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -81,6 +89,7 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_TIRADO,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_08,
         .flag    = EVFLAG_DEFEAT_BOSS,
@@ -88,13 +97,14 @@ const struct DefeatTalkEntNew gNewDefeatTalkList[] = {
     },
     {
         .pidA    = CHARACTER_BINKS,
+        .pidB    = 0xFFFF,
         .route   = CHAPTER_MODE_ANY,
         .chapter = CHAPTER_09,
         .flag    = EVFLAG_DEFEAT_BOSS,
         .msg     = MSG_DEFEAT_QUOTE_BINKS,
     },
     {
-        .pidA     = 0xffff,
+        .pidA    = 0xFFFF,
         .chapter = 0x00,
         .msg     = 0x0000,
     },
@@ -115,12 +125,16 @@ struct DefeatTalkEntNew* GetDefeatTalkEntry(u16 pidA) {
             continue;
         }
 
-        if ((pidA == it->pidA) && (
-                it->pidB == 0xFFFF ||
-                (GetUnit(gBattleActor.unit.index)->pCharacterData->number == it->pidB) ||
-                (GetUnit(gBattleTarget.unit.index)->pCharacterData->number == it->pidB)
-            )) {
-            return (struct DefeatTalkEntNew *)it;
+        if (pidA == it->pidA) {
+            // If pidB is not set (wildcard), match any opponent
+            if (it->pidB == 0xFFFF) {
+                return (struct DefeatTalkEntNew *)it;
+            }
+            // Otherwise, check if either actor or target matches pidB
+            if ((GetUnit(gBattleActor.unit.index)->pCharacterData->number == it->pidB) ||
+                (GetUnit(gBattleTarget.unit.index)->pCharacterData->number == it->pidB)) {
+                return (struct DefeatTalkEntNew *)it;
+            }
         }
     }
 
