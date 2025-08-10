533871a076773d838cd2b351e4255359f7a09768
JesterWizard <adapps101@gmail.com>
2025-08-10 21:40:20 +0100

    RE-ADDED - VeslyDangerBones

---
+++ Kernel/Wizardry/Misc/VeslyDangerBones/C_Code.c
@@ -0,0 +1,341 @@
+#include "C_Code.h"
+
+#define brk asm("mov r11, r11");
+#define DangerBonesBufferSize 0x2878
+#define enemySize (50 + 7) >> 3
+#define MaxTiles DangerBonesBufferSize / enemySize
+// x*y = 1480 or smaller eg. up to around a 38x38 map
+
+#define DangerBonesBuffer ((u8*)0x201c8d0)
+
+// extern u8 DangerBonesBuffer[DangerBonesBufferSize];
+
+#define US_BIT_SHAKE (1 << 24)
+#define US_BIT_PAL (1 << 27)
+
+// #define EMPTY_BmUnit
+
+// break point on buffer
+// [0x201c8d0..0x201c8d0+0x2878]!!
+
+extern int ShakeIt;
+extern int Pal_4th;
+extern int DangerBonesDisabledFlag;
+
+int ShouldDangerBonesNotRun(void)
+{
+    if (gPlaySt.faction)
+    {
+        return true;
+    }
+    if (CheckFlag(DangerBonesDisabledFlag))
+    {
+        return true;
+    }
+    return false;
+}
+
+int IsUnitInvalid(struct Unit * unit)
+{
+    if (!UNIT_IS_VALID(unit))
+    {
+        return true; // not a unit
+    }
+    if (gPlaySt.chapterVisionRange)
+    {
+        if (gBmMapFog[unit->yPos][unit->xPos] == 0)
+        {
+            return true; // in the fog
+        }
+    }
+
+    if (unit->state & US_UNDER_A_ROOF)
+    {
+        return true; // under a roof
+    }
+    return false;
+}
+
+void RemoveEnemyShaking(void)
+{
+    if (ShouldDangerBonesNotRun())
+    {
+        return;
+    }
+    struct Unit * unit;
+    int unitState = 0;
+    if (ShakeIt)
+    {
+        unitState |= US_BIT_SHAKE;
+    }
+    if (Pal_4th)
+    {
+        unitState |= US_BIT_PAL;
+    }
+
+    for (int i = 0x80; i < 0xC0; ++i)
+    {
+        unit = GetUnit(i);
+        if (IsUnitInvalid(unit))
+        {
+            continue;
+        }
+
+        unit->state &= ~unitState;
+    }
+    // RefreshUnitSprites(); // displays SMS under MMS when killing
+}
+
+void UpdateVisualsForEnemiesWhoCanAttackTile(void)
+{
+    int x = gBmSt.playerCursor.x;
+    int y = gBmSt.playerCursor.y;
+    SetLastCoords(x, y);             // vanilla
+    SetWorkingBmMap(gBmMapMovement); // vanilla
+    if (ShouldDangerBonesNotRun())
+    {
+        return;
+    }
+    u8 savedUnitId = gBmMapUnit[y][x];
+    gBmMapUnit[y][x] = 0;
+
+    int unitState = 0;
+    if (ShakeIt)
+    {
+        unitState |= US_BIT_SHAKE;
+    }
+    if (Pal_4th)
+    {
+        unitState |= US_BIT_PAL;
+    }
+
+    struct Unit * unit;
+    RemoveEnemyShaking();
+
+    int mightExceedBuffer = gBmMapSize.x * gBmMapSize.y > MaxTiles;
+
+    int deploymentID;
+    u8 * row = &DangerBonesBuffer[y * gBmMapSize.x * enemySize];
+
+    for (int i = 0; i < enemySize; i++) // Loop over bytes
+    {
+        if (mightExceedBuffer)
+        {
+            if (((y * gBmMapSize.x * enemySize) + (x * enemySize) + i) > DangerBonesBufferSize)
+            {
+                continue;
+            }
+        }
+        u8 byte = row[(x * enemySize) + i];
+        for (int bit = 0; bit < 8; bit++)
+        {
+            if (!(byte & (1 << bit)))
+            {
+                continue;
+            }
+            deploymentID = 0x80 + ((i << 3) + bit); // i*8 + bit
+
+            unit = GetUnit(deploymentID);
+            if (IsUnitInvalid(unit))
+            {
+                continue; // not a unit
+            }
+
+            unit->state |= unitState;
+        }
+    }
+
+    gBmMapUnit[y][x] = savedUnitId;
+    RefreshUnitSprites();
+}
+
+void CopyAttackRangeIntoBuffer(int i, int xSize, int ySize)
+{
+    int mightExceedBuffer = xSize * ySize > MaxTiles;
+    i &= 0x3F;
+    int byteID = i >> 3; // Which byte of the buffer to write to
+    i &= 7;
+    i = 1 << i; // Deployment ID - Bit to set at that byte
+    u8 * buf;
+    u8 * rangeMapY;
+
+    for (int y = 0; y < ySize; y++)
+    {
+        rangeMapY = gBmMapRange[y];
+        buf = &DangerBonesBuffer[y * xSize * enemySize];
+        for (int x = 0; x < xSize; x++)
+        {
+            if (!rangeMapY[x])
+            {
+                continue;
+            }
+            if (mightExceedBuffer)
+            {
+                if (((y * xSize * enemySize) + (x * enemySize) + byteID) > DangerBonesBufferSize)
+                {
+                    continue;
+                }
+            }
+            buf[(x * enemySize) + byteID] |= i;
+        }
+    }
+}
+
+typedef struct
+{
+    /* 00 */ PROC_HEADER;
+    /* 2c */ u8 id;
+} DangerBonesProc;
+
+extern void RefreshUnitsOnBmMap(void);
+void GenerateDangerBones(DangerBonesProc * proc) // do 1 valid unit per frame to spread out the lag
+{
+    u8 savedUnitId;
+    struct Unit * activeUnit = gActiveUnit;
+
+    int xSize = gBmMapSize.x;
+    int ySize = gBmMapSize.y;
+
+    int counter = 0;
+    if (proc->id >= 0xC0)
+    {
+        return;
+    }
+
+    for (int i = proc->id; i < 0xC0; ++i) // Enemy only
+    {
+        if (counter)
+        {
+            break;
+        }
+        struct Unit * unit = GetUnit(i);
+        if (proc->id == 0xBF)
+        {
+            BmMapFill(gBmMapRange, 0);
+            BmMapFill(gBmMapMovement, 0);
+        }
+        proc->id = i + 1;
+
+        if (IsUnitInvalid(unit))
+        {
+            continue;
+        }
+#ifdef EMPTY_BmUnit
+        BmMapFill(gBmMapUnit, 0);
+#endif
+        counter++;
+        gActiveUnit = unit;
+
+        BmMapFill(gBmMapRange, 0);
+        BmMapFill(gBmMapOther, 0);
+        GenerateUnitMovementMap(unit);
+
+        savedUnitId = gBmMapUnit[unit->yPos][unit->xPos];
+        gBmMapUnit[unit->yPos][unit->xPos] = 0;
+
+        SetWorkingBmMap(gBmMapRange);
+        // gWorkingBmMap 30049a0
+        GenerateUnitCompleteAttackRange(unit);
+        // SetWorkingBmMap(gBmMapRange);
+
+        CopyAttackRangeIntoBuffer(i & 0x3F, xSize, ySize);
+
+        gBmMapUnit[unit->yPos][unit->xPos] = savedUnitId;
+    }
+    gActiveUnit = activeUnit;
+#ifdef EMPTY_BmUnit
+    RefreshUnitsOnBmMap();
+#endif
+}
+
+const struct ProcCmd DangerBonesProcCmd[] = {
+    PROC_YIELD,
+    PROC_LABEL(0),
+    PROC_REPEAT(GenerateDangerBones),
+    PROC_END,
+};
+
+void GenerateDangerBonesRangeAll(int i) // Causes noticable lag if done for 0x80 - 0xBF at once
+{
+    // brk;
+    u8 savedUnitId;
+    struct Unit * activeUnit = gActiveUnit;
+    int xSize = gBmMapSize.x;
+    int ySize = gBmMapSize.y;
+#ifdef EMPTY_BmUnit
+    BmMapFill(gBmMapUnit, 0);
+#endif
+
+    for (; i < 0xC0; ++i) // Enemy only
+    {
+        struct Unit * unit = GetUnit(i);
+
+        if (IsUnitInvalid(unit))
+        {
+            continue;
+        }
+        gActiveUnit = unit;
+        BmMapFill(gBmMapRange, 0);
+        GenerateUnitMovementMap(unit);
+
+        savedUnitId = gBmMapUnit[unit->yPos][unit->xPos];
+        gBmMapUnit[unit->yPos][unit->xPos] = 0;
+
+        SetWorkingBmMap(gBmMapRange);
+        GenerateUnitCompleteAttackRange(unit);
+        CopyAttackRangeIntoBuffer(i & 0x3F, xSize, ySize);
+
+        gBmMapUnit[unit->yPos][unit->xPos] = savedUnitId;
+    }
+    BmMapFill(gBmMapRange, 0);
+    BmMapFill(gBmMapMovement, 0);
+    gActiveUnit = activeUnit;
+#ifdef EMPTY_BmUnit
+    RefreshUnitsOnBmMap();
+#endif
+    // brk;
+}
+
+void StartDangerBonesRange(void)
+{
+    if (ShouldDangerBonesNotRun())
+    {
+        return;
+    }
+    CpuFill16(0, DangerBonesBuffer, DangerBonesBufferSize);
+#ifdef EMPTY_BmUnit
+    BmMapFill(gBmMapUnit, 0);
+#endif
+
+    DangerBonesProc * proc = Proc_Find((ProcPtr)&DangerBonesProcCmd);
+    if (proc)
+    {
+        proc->id = 0x80;
+    }
+    else
+    {
+        proc = Proc_Start((ProcPtr)&DangerBonesProcCmd, PROC_TREE_3);
+        proc->id = 0x80;
+    }
+    // GenerateDangerBonesRange();
+}
+
+void FinishDangerBonesRange(void) // if proc didn't finish yet, calc the rest now
+{
+    if (ShouldDangerBonesNotRun())
+    {
+        return;
+    }
+    DangerBonesProc * proc = Proc_Find((ProcPtr)&DangerBonesProcCmd);
+    int id = 0x80;
+    if (proc)
+    {
+        id = proc->id;
+        proc->id = 0xC0; // stop proc from continuing
+    }
+    else
+    {
+        CpuFill16(0, DangerBonesBuffer, DangerBonesBufferSize);
+    }
+    GenerateDangerBonesRangeAll(id);
+}
---
+++ Kernel/Wizardry/Misc/VeslyDangerBones/Hooks.asm
@@ -0,0 +1,139 @@
+
+.thumb 
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+
+@ in 1CC1C
+.global Hook_PlayerPhase_InitUnitMovementSelect
+.type Hook_PlayerPhase_InitUnitMovementSelect, %function 
+Hook_PlayerPhase_InitUnitMovementSelect:
+push {lr} 
+ldrb r1, [r5, #4] 
+mov r0, #2 
+orr r0, r1 
+strb r0, [r5, #4] 
+
+bl FinishDangerBonesRange
+
+ldr r4, =0x3004e50 @gActiveUnit 
+ldr r0, [r4] 
+pop {r3} 
+bx r3 
+.ltorg 
+
+
+.global Hook_PlayerPhase_Suspend
+.type Hook_PlayerPhase_Suspend, %function 
+Hook_PlayerPhase_Suspend:
+push {lr} 
+bl StartDangerBonesRange
+ldr r1, =0x203a958 
+mov r0, #0
+strb r0, [r1, #0x16]
+mov r0, #3 
+ldr r3, =0x801C89F 
+bx r3 
+.ltorg 
+
+.global Hook_HandlePostActionTraps
+.type Hook_HandlePostActionTraps, %function 
+Hook_HandlePostActionTraps:
+push {lr} 
+push {r0}
+
+bl StartDangerBonesRange
+
+pop {r0}
+cmp r0, #0 
+ble Exit2
+
+ldr r2, [r4]
+ldr r0, [r2]
+ldr r1, [r2, #4] 
+ldr r0, [r0, #0x28]
+
+
+
+
+pop {r3} 
+bx r3 
+.ltorg 
+
+Exit2:
+pop {r3} 
+ldr r3, =0x803775d 
+bx r3 
+.ltorg 
+
+
+.global Hook_EnsureCameraOntoPosition
+.type Hook_EnsureCameraOntoPosition, %function 
+Hook_EnsureCameraOntoPosition: 
+push {lr} 
+lsl r0, #0x10 
+lsr r6, r0, #0x10 
+bl RemoveEnemyShaking
+ldr r1, =0x202BCb0 @ gGameState 
+mov r2, #0xC 
+ldsh r0, [r1, r2] 
+cmp r7, r0 
+
+pop {r3} 
+bx r3 
+.ltorg 
+
+.global Hook_RefreshUnitSprites
+.type Hook_RefreshUnitSprites, %function 
+Hook_RefreshUnitSprites: 
+push {lr} 
+lsl r2, r0, #16 
+lsr r2, #24 
+cmp r2, #0xC0 
+bge DoNothing 
+mov r2, #0x10 
+lsl r2, #8 
+add r0, r2 
+DoNothing: 
+strh r0, [r5, #8] 
+mov r2, r9 
+ldrh r0, [r2] 
+strb r0, [r5, #0xB] 
+@add r4, #8 
+@ldrb r0, [r4, #2] 
+pop {r3}
+ldr r3, =0x8027345 
+bx r3 
+.ltorg 
+
+
+
+.global Hook_PutUnitSpritesOam
+.type Hook_PutUnitSpritesOam, %function 
+Hook_PutUnitSpritesOam: 
+push {lr} 
+mov r3, #0 
+cmp r0, #0 
+beq Exit_PutUnitSpritesOam 
+blh 0x8000D28 @ GetGameClock 
+mov r3, r0 
+mov r0, #0x1
+ldr r1, =ShakeSpeed_Link 
+ldr r1, [r1] 
+lsl r0, r1 
+
+and r3, r0 
+lsr r3, r1 
+Exit_PutUnitSpritesOam: 
+
+pop {r2} 
+bx r2 
+.ltorg 
+
+
+
+
+
+
