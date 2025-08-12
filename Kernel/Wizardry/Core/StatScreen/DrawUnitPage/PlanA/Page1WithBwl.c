bfd22b5ca7a86a19575903dad626866b17b6193d
JesterWizard <adapps101@gmail.com>
2025-08-12 01:28:29 +0100

    RE-ADDED - StatScreenAllegianceColors

--- Kernel/Wizardry/Core/StatScreen/DrawUnitPage/PlanA/Page1WithBwl.c
+++ Kernel/Wizardry/Core/StatScreen/DrawUnitPage/PlanA/Page1WithBwl.c
@@ -324,7 +324,7 @@ STATIC_DECLAR void DrawPage1Affin(void)
 			GetUnitAffinityIcon(unit),
 			TILEREF(0, STATSCREEN_BGPAL_EXTICONS));
 
-		if (gpKernelDesigerConfig->== true) {
+		if (gpKernelDesigerConfig->use_chinese_character == true) {
 			PutDrawText(
 				&gStatScreen.text[STATSCREEN_TEXT_ITEM1],
 				gUiTmScratchA + TILEMAP_INDEX(0xE, 0x7),
