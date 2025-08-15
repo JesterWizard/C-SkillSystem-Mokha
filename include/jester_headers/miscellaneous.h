c5c0ef8da8535d2b2d56e741c52a895b299fd05d
JesterWizard <adapps101@gmail.com>
2025-08-15 22:28:43 +0100

    RE-ADDED - QuintessanceEffect

---
+++ include/jester_headers/miscellaneous.h
@@ -0,0 +1,58 @@
+#pragma once
+
+/**
+ * Quintessance stealing effect functions
+ * Disabled some externs because of compile warnings about "useless storage class specifier in empty declaration"
+ */
+extern const struct ProcCmd ProcScr_QuintessenceFx[];
+//extern struct QuintessenceFxProc;
+extern void fe7j_sub_80781C8(u16 * buf, s16 phase, s16 amplitude, s16 frequency, s16 param_5, s16 param_6, s16 param_7);
+extern u16 * fe7j_sub_8077CEC(int buf_id, int scanline);
+extern void QuintessenceFx_OnHBlank(void);
+//extern struct Proc08C01654;
+//extern void fe7j_sub_80120D8(struct Proc08C01654 * proc);
+//extern void fe7j_sub_80120E0(struct Proc08C01654 * proc);
+extern const struct ProcCmd ProcScr_FE7J_08C01654;
+extern void fe7j_sub_8012150(int a, int b, int c, int d, int e, ProcPtr parent);
+extern void FE7J_EventE5_Sim_Type_A(ProcPtr proc);
+extern void FE7J_EventE5_Sim_Type_B(ProcPtr proc);
+extern void fe7j_sub_80121FC(void);
+//extern void QuintessenceFx_ParallelWorker(struct QuintessenceFxProc * proc);
+//extern void QuintFxBg2_Init(struct QuintessenceFxProc * proc);
+//extern void QuintFxBg2_Loop(struct QuintessenceFxProc * proc);
+extern const struct ProcCmd ProcScr_QuintessenceFxBg2Scroll;
+//extern void QuintessenceFx_Init_Main(struct QuintessenceFxProc * proc);
+//extern void QuintessenceFx_Loop_A(struct QuintessenceFxProc * proc);
+//extern void QuintessenceFx_ResetBlend(struct QuintessenceFxProc * proc);
+//extern void QuintessenceFx_Loop_B(struct QuintessenceFxProc * proc);
+//extern void QuintessenceFx_Loop_C(struct QuintessenceFxProc * proc);
+extern void QuintessenceFx_OnEnd(void);
+extern void StartQuintessenceStealEffect(struct Proc * parent);
+extern void QuintessenceFx_Goto_B(void);
+extern void QuintessenceFx_Goto_C(void);
+extern void EndQuintessenceStealEffect(void);
+
+extern void GiveScroll(void);
+
+extern void IsTraineeLevelCappedOrPromoted(void);
+
+extern void TryAddUnitToAdjacentEnemyTargetList(struct Unit* unit);
+extern void MakeTargetListForAdjacentEnemies(struct Unit* unit);
+
+extern void TryAddUnitToAdjacentEnemyNonBossTargetList(struct Unit* unit);
+extern void MakeTargetListForAdjacentNonBossEnemies(struct Unit* unit);
+
+extern void TryAddUnitToAdjacentSameFactionTargetList(struct Unit* unit);
+extern void MakeTargetListForAdjacentSameFaction(struct Unit* unit);
+
+extern void TryAddUnitToAdjacentUnitsTargetList(struct Unit* unit);
+extern void MakeTargetListForAdjacentUnits(struct Unit* unit);
+
+extern void TryAddUnitToRangedStatusStavesTargetList(struct Unit* unit);
+extern void MakeTargetListForRangedStatusStaves(struct Unit* unit);
+
+struct BEXP {
+    u16 value;
+};
+
+extern struct BEXP sBEXP;
