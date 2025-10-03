e613e0159d59377410b072f0d9b52466d938ef3f
JesterWizard <adapps101@gmail.com>
2025-10-03 12:46:40 +0100

    Fixed JesterWizard/C-SkillSystem-Mokha#342

--- Data/Shield/ShieldConfig.c
+++ Data/Shield/ShieldConfig.c
@@ -6,45 +6,22 @@ struct ShieldExtEquipableConfig const gShieldExtEquipableConfig[] = {
 };
 
 struct ShieldItem const gShieldItemTable[0x100] = {
-	[ITEM_HOPLON_SHIELD] = {
-		.en = true,
+	// [ITEM_HOPLON_SHIELD] = {
+	// 	.en = true,
 
-		.info = {
-			.iid = ITEM_HOPLON_SHIELD,
-			.consume_on_defense = true,
-			.consume_on_hitted  = true,
-			.battle_status_bonus = { [BATTLE_STATUS_DEF] = 2 },
-			.unit_status_bonus = {
-				[UNIT_STATUS_DEF] = 3,
-				[UNIT_STATUS_RES] = 3,
-			},
-			.unit_growth_bonus = {
-				[UNIT_STATUS_DEF] = 10,
-				[UNIT_STATUS_RES] = 10,
-			},
-		},
-	},
-
-#ifdef CONFIG_USE_DEBUG
-	[ITEM_ELIXIR] = {
-		.en = true,
-
-		.info = {
-			.iid = ITEM_ELIXIR,
-			.consume_on_attack = true,
-			.consume_on_hitted = true,
-			.battle_status_bonus = { [BATTLE_STATUS_ATK] = 2 },
-		},
-	},
-	[ITEM_VULNERARY] = {
-		.en = true,
-
-		.info = {
-			.iid = ITEM_VULNERARY,
-			.consume_on_attack = true,
-			.consume_on_hitted = true,
-			.battle_status_bonus = { [BATTLE_STATUS_ATK] = 2 },
-		},
-	},
-#endif
+	// 	.info = {
+	// 		.iid = ITEM_HOPLON_SHIELD,
+	// 		.consume_on_defense = true,
+	// 		.consume_on_hitted  = true,
+	// 		.battle_status_bonus = { [BATTLE_STATUS_DEF] = 2 },
+	// 		.unit_status_bonus = {
+	// 			[UNIT_STATUS_DEF] = 3,
+	// 			[UNIT_STATUS_RES] = 3,
+	// 		},
+	// 		.unit_growth_bonus = {
+	// 			[UNIT_STATUS_DEF] = 10,
+	// 			[UNIT_STATUS_RES] = 10,
+	// 		},
+	// 	},
+	// },
 };
