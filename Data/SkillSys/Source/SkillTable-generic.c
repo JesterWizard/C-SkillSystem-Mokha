d582cb81c9d3016951189febfbdd58de0addca64
JesterWizard <adapps101@gmail.com>
2025-10-26 10:48:27 +0000

    Skill - Dark Bargain

--- Data/SkillSys/Source/SkillTable-generic.c
+++ Data/SkillSys/Source/SkillTable-generic.c
@@ -10,7 +10,7 @@
 const struct SkillPreloadPConf gSkillPreloadPData[0x100] = {
     // 01
     [CHARACTER_EIRIKA] = {{
-        [0] = SID_Ruinator
+        [0] = SID_DarkBargain
     }},
     // 02
     [CHARACTER_SETH] = {{
--- Kernel/Wizardry/Misc/SkillEffects/PostActionSkills/Heal.c
+++ Kernel/Wizardry/Misc/SkillEffects/PostActionSkills/Heal.c
@@ -12,23 +12,81 @@ bool PostAction_BattleActorHeal(ProcPtr parent)
 
 	int hp_cur = GetUnitCurrentHp(gActiveUnit);
 	int hp_max = GetUnitMaxHp(gActiveUnit);
+    int missingHP = hp_max - hp_cur;
 
 	if (!UnitAvaliable(gActiveUnit) || UNIT_STONED(gActiveUnit))
 		return false;
 
 #if defined(SID_Lifetaker) && (COMMON_SKILL_VALID(SID_Lifetaker))
-	if (SkillListTester(gActiveUnit, SID_Lifetaker) && gBattleActorGlobalFlag.enemy_defeated)
+	if (SkillTester(gActiveUnit, SID_Lifetaker) && gBattleActorGlobalFlag.enemy_defeated)
 		heal += perc_of(hp_max, SKILL_EFF0(SID_Lifetaker));
 #endif
 
 #if defined(SID_MysticBoost) && (COMMON_SKILL_VALID(SID_MysticBoost))
-	if (SkillListTester(gActiveUnit, SID_MysticBoost))
+	if (SkillTester(gActiveUnit, SID_MysticBoost))
 		heal += SKILL_EFF0(SID_MysticBoost);
 #endif
 
 #if defined(SID_BoundlessVitality) && (COMMON_SKILL_VALID(SID_BoundlessVitality))
-	if (SkillListTester(gActiveUnit, SID_BoundlessVitality))
+	if (SkillTester(gActiveUnit, SID_BoundlessVitality))
 		heal += perc_of(hp_max, SKILL_EFF0(SID_BoundlessVitality));
+#endif
+
+#if defined(SID_DarkBargain) && (COMMON_SKILL_VALID(SID_DarkBargain))
+    if (SkillTester(gActiveUnit, SID_DarkBargain))
+    {
+        if (gActiveUnit->exp >= missingHP)
+        {
+            gActiveUnit->exp -= missingHP;
+            heal += missingHP;
+        }
+        else
+        {
+            heal += gActiveUnit->exp;
+            gActiveUnit->exp = 0;
+        }
+    }
+#endif
+
+#if defined(SID_HoardersBane) && (COMMON_SKILL_VALID(SID_HoardersBane))
+    if (SkillTester(gActiveUnit, SID_HoardersBane))
+    {
+        if (UNIT_CATTRIBUTES(gActiveUnit) & CA_SUPPLY || SkillTester(gActiveUnit, SID_HoardersBane))
+        {
+            if (gActiveUnit->curHP < gActiveUnit->maxHP)
+            {
+                if (GetConvoyItemSlot(ITEM_VULNERARY) != -1)
+                {
+                    u16 *convoy;
+                    convoy = GetConvoyItemArray();
+                    u16 usedItem = GetItemAfterUse(convoy[GetConvoyItemSlot(ITEM_VULNERARY)]);
+                    heal += SKILL_EFF0(SID_HoardersBane);
+                    RemoveItemFromConvoy(GetConvoyItemSlot(ITEM_VULNERARY));
+
+                    if (ITEM_USES(usedItem) != 0)
+                        AddItemToConvoy(usedItem);
+                }
+            }
+        }
+    }
+#endif
+
+#if defined(SID_Lifefont) && (COMMON_SKILL_VALID(SID_Lifefont))
+    if (SkillTester(gActiveUnit, SID_Lifefont))
+    {
+        if (gActiveUnit->curHP == gActiveUnit->maxHP)
+        {
+            switch (gBmMapTerrain[gActiveUnit->yPos][gActiveUnit->xPos]) {
+                case TERRAIN_FORT:
+                case TERRAIN_THRONE:
+                    heal += (GetTerrainHealAmount(gBmMapTerrain[gActiveUnit->yPos][gActiveUnit->xPos]) * gActiveUnit->maxHP / 100);
+                    break;
+        
+                default:
+                    break;
+            }
+        }
+    }
 #endif
 
 #ifdef CONFIG_RESTORE_HP_ON_LEVEL_UP
@@ -36,8 +94,8 @@ bool PostAction_BattleActorHeal(ProcPtr parent)
         heal = gActiveUnit->maxHP - gActiveUnit->curHP;
 #endif
 
-	if ((heal >= (hp_max - hp_cur)))
-		heal = hp_max - hp_cur;
+	if ((heal >= missingHP))
+		heal = missingHP;
 
 	if (heal <= 0)
 		return false;
