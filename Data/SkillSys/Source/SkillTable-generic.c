e5d14191215797a9ce6d7c0b815d3cd2d6a7abc3
JesterWizard <adapps101@gmail.com>
2025-10-30 22:01:15 +0000

    Skill - Moody

--- Data/SkillSys/Source/SkillTable-generic.c
+++ Data/SkillSys/Source/SkillTable-generic.c
@@ -10,7 +10,7 @@
 const struct SkillPreloadPConf gSkillPreloadPData[0x100] = {
     // 01
     [CHARACTER_EIRIKA] = {{
-        [0] = SID_PsychUp
+        [0] = SID_Moody
     }},
     // 02
     [CHARACTER_SETH] = {{
