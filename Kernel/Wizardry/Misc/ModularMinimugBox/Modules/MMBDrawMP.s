36cece805919bb2701e2aa4108786d672c78186b
JesterWizard <adapps101@gmail.com>
2025-11-13 19:23:25 +0000

    Fix JesterWizard/C-SkillSystem-Mokha#363

--- Kernel/Wizardry/Misc/ModularMinimugBox/Modules/MMBDrawMP.s
+++ Kernel/Wizardry/Misc/ModularMinimugBox/Modules/MMBDrawMP.s
@@ -147,51 +147,51 @@ SkipDashedCurrentMP:
 
 	add		r6, r6, #25
 
-	@ @ BWL - Max MP
-	@ @ 1. Get unit pointer for the attacker
-	@ @ 2. Get BWL entry 0xD for that unit
-	@ @ This is the old system that requires a specific BWL entry
+	@ BWL - Max MP
+	@ 1. Get unit pointer for the attacker
+	@ 2. Get BWL entry 0xD for that unit
+	@ This is the old system that requires a specific BWL entry
 
-	@ mov 	r0, r5
-	@ ldr 	r0, [r0, #0] 		@ Load class data of unit
-	@ ldrb 	r0, [r0, #4] 		@ Load character index
-	@ ldr 	r1, =BWL_GetEntry
-	@ mov 	lr, r1
-	@ bllr
-	@ mov 	lr, r3
-	@ cmp 	r0, #0
-	@ beq 	NoBWLData2
-	@ add 	r0, #UnitMaxMP
-	@ ldrb 	r0, [r0]
-	@ b 		CheckLimit2
-
-	@ NoBWLData2:
-	@ mov 	r0, #0
-
-	@ CheckLimit2:
-	@ cmp		r0, #254 // JESTER - Originally 99, but expanded as I have made HP go up to 254
-	@ ble		SkipDashedMaxMP
-
-	@ @ HP too high, display --
-
-	@ mov		r0, #0xFF
-
-	@ This is the new system that uses a custom MP getter derived from a custom fe8-kernel-dev.h header file
-	mov		r2, r0
-	mov		r0, r6
-	mov		r1, r7
-
-	mov		r0, r5
-	ldr		r1, =GetUnitMaxMP
-	mov		lr, r1
+	mov 	r0, r5
+	ldr 	r0, [r0, #0] 		@ Load class data of unit
+	ldrb 	r0, [r0, #4] 		@ Load character index
+	ldr 	r1, =BWL_GetEntry
+	mov 	lr, r1
 	bllr
+	mov 	lr, r3
+	cmp 	r0, #0
+	beq 	NoBWLData2
+	add 	r0, #UnitMaxMP
+	ldrb 	r0, [r0]
+	b 		CheckLimit2
 
+	NoBWLData2:
+	mov 	r0, #0
+
+	CheckLimit2:
 	cmp		r0, #254 // JESTER - Originally 99, but expanded as I have made HP go up to 254
 	ble		SkipDashedMaxMP
 
 	@ HP too high, display --
 
 	mov		r0, #0xFF
+
+	@ This is the new system that uses a custom MP getter derived from a custom fe8-kernel-dev.h header file
+	@ mov		r2, r0
+	@ mov		r0, r6
+	@ mov		r1, r7
+
+	@ mov		r0, r5
+	@ ldr		r1, =GetUnitMaxMP
+	@ mov		lr, r1
+	@ bllr
+
+	@ cmp		r0, #254 // JESTER - Originally 99, but expanded as I have made HP go up to 254
+	@ ble		SkipDashedMaxMP
+
+	@ @ HP too high, display --
+
+	@ mov		r0, #0xFF
 
 SkipDashedMaxMP:
 
